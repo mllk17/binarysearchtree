efa8180839c9fc57d79109d302c72735aabb9192
mllk17 <77943915+mllk17@users.noreply.github.com>
2021-01-24 20:40:55 -0600

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node{
+    public int data;
+    public Node left;
+    public Node right;
+    public Node(int key){
+        data = key;
+        left = null;
+        right = null;
+    }
+}
